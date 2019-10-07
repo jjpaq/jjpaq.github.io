8a4878d5886ef2f4e825939d8e2ab6517e4ba196
jjpaq <55399772+jjpaq@users.noreply.github.com>
2019-10-07 15:04:21 -0400

    added more inline stlye

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 
 <?php
-echo "<p style='border:\"1px solid black\";text-align:center;'>Third Test</p>";
+echo "<p style='dwidth:500px;background-color:blue;border:\"1px solid black\";text-align:center;'>Third Test</p>";
 
 ?>
