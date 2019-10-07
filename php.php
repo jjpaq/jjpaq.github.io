66ced16d6b4b0f3cd264a2a4cc6b056a7c57318c
jjpaq <55399772+jjpaq@users.noreply.github.com>
2019-10-07 15:34:30 -0400

    changes

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 
 <?php
-echo "<p style='width:500px;background-color:lightgrey;border:1px solid black;text-align:center;'>Third Test</p>";
+echo "<p style='margin:auto;width:500px;background-color:lightgrey;border:1px solid black;text-align:center;'>Third Test</p>";
 
 ?>
