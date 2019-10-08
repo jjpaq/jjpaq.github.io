2244bc4a2181025cd91218c6247fbb54fa54bd9c
jjpaq <55399772+jjpaq@users.noreply.github.com>
2019-10-07 23:16:08 -0400

    added php login script

--- php.php
+++ php.php
@@ -4,8 +4,48 @@
 </head>
 <body>
     <p id="c4_form_preview_name">
-        <?php echo $_POST["name"] ?>
+        <?php
+        $dbh = new PDO('sqlite:test.db') or die("cannot open the database");
+        if($_POST["name"])
+        {
+            // echo $_POST["name"];
+        
+            
+            //$query =  "SELECT * FROM accounts WHERE username=\"" . $_POST["name"] . "\";";
+            $query =  "SELECT * FROM accounts WHERE username LIKE '" . $_POST['name'] . "'";
 
+            $res = $dbh->query($query);
+
+            $numColumns = $res->fetchColumn();
+
+            //echo "Password: ", $pass, "\n";
+
+            if($numColumns == 1)
+            {
+                //echo("Got this far\n\n <br>");
+
+                foreach($dbh->query($query) as $res) 
+                {
+                    if(password_verify($_POST["password"],$res['password_hash']))
+                    {
+                        echo "Good login.";
+                    }
+                    else
+                    {
+                        echo "Incorrect password.";
+                    }
+                }
+            }
+            else
+            {
+                echo("Incorrect user name.");
+            }
+            
+            //var_dump($res);
+
+            $dbh = null;
+        }
+        ?>
     </p>
 
 </body>
