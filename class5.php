9e6c2bb4ced6f5b056127c9298c3a91cae96fbf1
jjpaq <55399772+jjpaq@users.noreply.github.com>
2019-10-21 16:03:01 -0400

    testing proc_ope

--- class5.php
+++ class5.php
@@ -6,7 +6,42 @@
 <body>
     Content:<br>    
     <?php
-        //echo $_POST["py-content"];
+        $descriptorspec = array(
+            0 => array("pipe", "r"),
+            1 => array("pipe", "w"),
+            2 => array("file", "error-output.txt", "a")
+         );
+         
+         $process = proc_open('new.exe', $descriptorspec, $pipes);
+         
+         $input = 20;
+         $exp = 2;
+         if (is_resource($process)) 
+         {
+             print fgets($pipes[1]);
+             fwrite($pipes[0], $input . "\n");
+         
+             print fgets($pipes[1]);
+             fwrite($pipes[0], $exp . "\n");
+         
+             print fgets($pipes[1]);
+         
+             fclose($pipes[1]);
+             fclose($pipes[0]);
+             $return_value = proc_close($process);
+         
+             echo "command returned $return_value\n";
+         }
+         else 
+         {
+             echo "No resource available";
+         }
+    ?>
+</body>
+
+</html>
+<!--
+//echo $_POST["py-content"];
 
         //$py_output = escapeshellcmd('/usr/local/bin/python3 print(\'it works!\')');
 
@@ -15,7 +50,4 @@
         $py_output = shell_exec($py_command);
 
         echo $py_output;
-    ?>
-</body>
-
-</html>
+-->
