4d09c7ecb8d1be428b21d6fbecc2beaa723de949
jjpaq <55399772+jjpaq@users.noreply.github.com>
2019-10-21 15:35:29 -0400

    added python input

--- class5.php
+++ class5.php
@@ -4,9 +4,17 @@
 <head></head>
 
 <body>
-    Content:<br>
+    Content:<br>    
     <?php
-        echo $_POST["py-content"];
+        //echo $_POST["py-content"];
+
+        //$py_output = escapeshellcmd('/usr/local/bin/python3 print(\'it works!\')');
+
+        file_put_contents("/home/public/php/php_test.py",$_POST["py-content"]);
+        $py_command = escapeshellcmd('python3 /home/public/php/php_test.py');
+        $py_output = shell_exec($py_command);
+
+        echo $py_output;
     ?>
 </body>
 
